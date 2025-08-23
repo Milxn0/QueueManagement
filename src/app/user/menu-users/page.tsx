df4777b760a7767e88faa19da30b861598d87766
thatphong434 <6710210434@psu.ac.th>
2025-08-23 15:25:18 +0700

    error/cardmenu

---
+++ src/app/user/menu-users/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-window-100">
+      <h1 className="text-2xl font-bold text-purple my-4">User Menu</h1>
+
+      
+      
+      {/* <div className="w-full max-w-6xl px-4">
+        <img
+          src="\menu.jpg"
+          alt="Seoul BBQ Menu"
+          className="w-full h-auto rounded-lg shadow-lg"
+        />
+      </div> */}
+
+
+
+    </div>
+  );
+}
